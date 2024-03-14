9c9ea7de7347a465f13ecb0ddb7eaa321914255b
FruzsiH <fruzs.horvath@gmail.com>
2024-03-14 09:21:16 +0100

    add examples

--- src/main/java/hu/webler/set/LinkedHashSetExample.java
+++ src/main/java/hu/webler/set/LinkedHashSetExample.java
@@ -1,16 +1,15 @@
 package hu.webler.set;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
-
-public class HashSetExample {
+public class LinkedHashSetExample {
 
     public static void main(String[] args) {
 
-        Set<Integer> numbers = new HashSet<>();
+        Set<Integer> numbers = new LinkedHashSet<>();
         numbers.add(10);
-        numbers.add(20);
         numbers.add(30);
+        numbers.add(20);
         numbers.add(30);
 
         for (Integer num : numbers) {
